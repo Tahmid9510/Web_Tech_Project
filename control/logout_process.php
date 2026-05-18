07a6a0e62d8e644267e8697e79292d1972df12b6
Tahmid Hasan <tmd999510@gmail.com>
2026-05-18 23:17:09 +0600

    Logout Processes added

---
+++ control/logout_process.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+session_unset();
+session_destroy();
+
+if (isset($_COOKIE["remember_token"])) {
+    setcookie("remember_token", "", time() - 3600, "/");
+}
+
+header("Location: ../view/login.php");
+exit();
+
+?>
